6cfe8821b18ac681540e906e1949387aaa8725c6
zhangjian <13864264792@163.com>
2020-04-17 15:12:45 +0800

    x

--- lib/get_token.py
+++ lib/get_token.py
@@ -1,14 +1,25 @@
 import requests
+import json
 
 
 class Req():
+
+    token = {}
+
     @staticmethod
-    def get_token():
+    def update_token():
         ret = requests.get(
             "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx963cab3f76c2a895&secret=607dee317bb50fab9196bb9c06cc3c1a")
-        print(ret.text)
+        if ret.status_code == 0:
+            Req.token = json.loads(ret.text)
         return ret
+
+    @classmethod
+    def give_token(cls):
+        return cls.token.get("access_token")
 
 
 if __name__ == '__main__':
-    Req.get_token()
+    print(Req.token)
+    Req.update_token()
+    print(Req.token)
