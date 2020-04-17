21714dfcf80754095d09a99657a91b5c47fb81db
zhangjian <13864264792@163.com>
2020-04-17 15:02:36 +0800

    x

--- lib/get_token.py
+++ lib/get_token.py
@@ -4,5 +4,5 @@ import requests
 class Req():
     def get_token(self):
         ret = requests.get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx963cab3f76c2a895&secret=607dee317bb50fab9196bb9c06cc3c1a")
-        print(ret)
+        print(ret.text)
         return ret
