71099832de977884c8efb18ac8ae7788c692c225
zhangjian <13864264792@163.com>
2020-04-17 15:14:17 +0800

    x

--- lib/get_token.py
+++ lib/get_token.py
@@ -10,6 +10,7 @@ class Req():
     def update_token():
         ret = requests.get(
             "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx963cab3f76c2a895&secret=607dee317bb50fab9196bb9c06cc3c1a")
+        print(ret.status_code)
         if ret.status_code == 0:
             Req.token = json.loads(ret.text)
         return ret
